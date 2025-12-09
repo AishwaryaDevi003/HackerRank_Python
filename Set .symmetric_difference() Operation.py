aecef30be3181a74619b9763479e47c4bd1d45d5
AishwaryaDevi003 <113661175+AishwaryaDevi003@users.noreply.github.com>
2025-12-09 18:54:47 +0530

    Create Set .symmetric_difference() Operation.py

---
+++ Set .symmetric_difference() Operation.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+n=int(input())
+l=set(map(int,input().split()))
+n1=int(input())
+l1=set(map(int,input().split()))
+print(len(l.symmetric_difference(l1)))
