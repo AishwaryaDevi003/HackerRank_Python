28c7f7df6e1619f9281322b974d89a21a3a783dc
AishwaryaDevi003 <113661175+AishwaryaDevi003@users.noreply.github.com>
2025-12-09 18:52:37 +0530

    Add string compression functionality
    Implement string compression using groupby.

---
+++ Compress the String!.py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+from itertools import groupby
+
+a = input()
+b = [int(i) for i in a]
+
+for x, y in groupby(b):
+    n = len(list(y))
+    print((n, x), end=" ")
