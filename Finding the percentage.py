97d8412f99fcfa496deb2e4b8d8409772a685594
AishwaryaDevi003 <113661175+AishwaryaDevi003@users.noreply.github.com>
2025-12-09 10:53:52 +0530

    Implement student percentage calculation

---
+++ Finding the percentage.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    for i,k in student_marks.items():
+        if i == query_name:
+            k= sum(k)/len(k)
+            print(f'{k:.2f}')
+        
