4776fd576e3bd11075ce7bedf75853839630be40
AishwaryaDevi003 <113661175+AishwaryaDevi003@users.noreply.github.com>
2025-12-09 18:24:14 +0530

    Create Capitalize!.py

---
+++ Capitalize!.py
@@ -0,0 +1,10 @@
+
+
+# Complete the solve function below.
+def solve(s):
+    words=s.split(" ")   
+    for i in range(len(words)):
+        words[i] = words[i].capitalize()
+            
+    return ' '.join(words)
+
